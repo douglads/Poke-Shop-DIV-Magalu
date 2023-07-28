b97d3a2fa9cda035348a68196a25d00aa889d43f
Douglas Fernandes Machado <douglas.machado@luizalabs.com>
2023-07-28 18:49:54 -0300

    Refatorando 404

--- src/screens/Page404/index.tsx
+++ src/screens/Page404/index.tsx
@@ -1,16 +1,11 @@
-import { Link } from "react-router-dom";
 import Header from "../../components/Header";
-import PokemonBallCount from "../../components/PokemonBallCount";
 import { H1 } from "../Cart/styles";
-import logo from '../../assets/logo.png'
-import { useContext, useState, useEffect } from "react"
-import { CartItemsContext } from "../../Context/CartItems"
+import { useState, useEffect } from "react"
 import { Footer } from "../../components/Footer";
 import { Main404 } from "./styles";
 import { useNavigate } from "react-router-dom";
 
 export function Page404() {
-    const { CartItems } = useContext(CartItemsContext)
     const [time, setTime] = useState(5)
     const redirect = useNavigate()
     useEffect(() => {
@@ -27,15 +22,7 @@ export function Page404() {
     return (
         <>
             <Header>
-                <section>
-                    <Link to='/'>
-                        <img className='logo' src={logo} alt="" />
-                    </Link>
-                </section>
-                <section className="center">
-                    <H1>Osh... Essa página não existe</H1>
-                </section>
-                <PokemonBallCount value={CartItems.count} />
+                <H1>Osh... Essa página não existe</H1>
             </Header>
             <Main404>
                 <p>Você será redirecionado em {time} segundos para a tela inicial</p>
